3cbf414a310095930d49b5d413d06171a4b3ec8a
Kristo Truu <kristo.truu@gmail.com>
2020-08-02 14:08:23 +0300

    Updates

---
+++ precache-manifest.3761197984bfbadd9f5bb9e627647894.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c12c0ff5111616e1ec3761418cd1a7",
+    "url": "/idcheck/index.html"
+  },
+  {
+    "revision": "3cdf31b90755826f0fc5",
+    "url": "/idcheck/static/css/2.93b3b32d.chunk.css"
+  },
+  {
+    "revision": "b23efeb2c7d5521c8cb0",
+    "url": "/idcheck/static/css/main.9d78fa5b.chunk.css"
+  },
+  {
+    "revision": "3cdf31b90755826f0fc5",
+    "url": "/idcheck/static/js/2.be232c6d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/idcheck/static/js/2.be232c6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b23efeb2c7d5521c8cb0",
+    "url": "/idcheck/static/js/main.c3f5e48f.chunk.js"
+  },
+  {
+    "revision": "11902ddda882b9ddf0b7",
+    "url": "/idcheck/static/js/runtime-main.e1c5c11a.js"
+  },
+  {
+    "revision": "e62b3130a1faa3434353e3c7f5ff2664",
+    "url": "/idcheck/static/media/Prompt-Black.e62b3130.ttf"
+  },
+  {
+    "revision": "befa1f27b3fbec48e74d58e8f7d61bb8",
+    "url": "/idcheck/static/media/Prompt-Bold.befa1f27.ttf"
+  },
+  {
+    "revision": "2aed5273600290713f7ed8d182a93592",
+    "url": "/idcheck/static/media/Prompt-Light.2aed5273.ttf"
+  },
+  {
+    "revision": "16b4ce72cf30da14d83d5e3981d3113c",
+    "url": "/idcheck/static/media/Prompt-Regular.16b4ce72.ttf"
+  },
+  {
+    "revision": "ddcfbf895b67206d003167df83c7020a",
+    "url": "/idcheck/static/media/Prompt-Thin.ddcfbf89.ttf"
+  }
+]);
