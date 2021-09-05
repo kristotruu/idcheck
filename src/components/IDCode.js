87afbdca69705d6996e2134809a6f162923647c3
Kristo Truu <kristo.truu@gmail.com>
2021-09-05 15:41:48 +0300

    style update

--- src/components/IDCode.js
+++ src/components/IDCode.js
@@ -12,19 +12,20 @@ export default ({idCode}) => {
         dispatch(showToast("Copied", `Identification code ${code} has been copied`));
     }
     return (
-        <Row key={idCode.code} className="generated">
-            <Col md={6}>{idCode.code} </Col>
-            {idCode.gender && <Col md={2}>
-                <span className="badge">{idCode.gender === 1 ? 'MALE' : 'FEMALE'}</span>
-            </Col>
-            }
-            <Col md={2}>
-                <span className="badge badge-dark">{idCode.date.format('YYYY-MM-DD')}</span>
-            </Col>
-            <Col md={2} className="d-flex justify-content-end align-items-center">
-                <CopyToClipboard text={idCode.code} onCopy={onCopy} >
-                    <MdContentCopy/>
-                </CopyToClipboard></Col>
-        </Row>
+        <CopyToClipboard text={idCode.code} onCopy={onCopy} >
+            <Row key={idCode.code} className="generated">
+                <Col md={6}>{idCode.code} </Col>
+                {idCode.gender && <Col md={2}>
+                    <span className="badge">{idCode.gender === 1 ? 'MALE' : 'FEMALE'}</span>
+                </Col>
+                }
+                <Col md={2}>
+                    <span className="badge badge-dark">{idCode.date.format('YYYY-MM-DD')}</span>
+                </Col>
+                <Col md={2} className="d-flex justify-content-end align-items-center">
+                        <MdContentCopy/>
+                </Col>
+            </Row>
+        </CopyToClipboard>
     )
 }
